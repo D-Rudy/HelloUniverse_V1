e8bec8f96162b22d48232a85401c2a4527186aba
D-Rudy <duriez.ry@gmail.com>
2024-03-06 16:11:15 +0100

    Affectation de variables

--- com.rdz.HelloUniverse/src/com/rdz/HelloUniverse/HelloUniverse.java
+++ com.rdz.HelloUniverse/src/com/rdz/HelloUniverse/HelloUniverse.java
@@ -2,7 +2,14 @@ package com.rdz.HelloUniverse;
 
 public class HelloUniverse {
 	public static void main(String[] args) {
-		System.out.println("Hello Universe !");
+
+		int planete = 8;
+		
+		System.out.println("Aux dernières nouvelles, le nombre total de planètes dans le système solaire est de :");
+		System.out.println(planete);
+		System.out.println("Il y a quelques années cependant, elles étaient au nombre de :");
+		planete += 1;
+		System.out.println(planete);
 	}
 
 }
