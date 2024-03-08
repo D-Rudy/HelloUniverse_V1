b5d373e17ddd0f7778f5bc602f0ac2a62260053b
D-Rudy <duriez.ry@gmail.com>
2024-03-08 15:38:45 +0100

    if

--- com.rdz.HelloUniverse/src/com/rdz/HelloUniverse/HelloUniverse.java
+++ com.rdz.HelloUniverse/src/com/rdz/HelloUniverse/HelloUniverse.java
@@ -3,11 +3,20 @@ package com.rdz.HelloUniverse;
 public class HelloUniverse {
 	public static void main(String[] args) {
 
-		int nbPlanete = 8;
-		String texte1 = "Aux dernières nouvelles, le nombre total de planètes dans le système solaire est de : ";
-		String texte2 = "\n Il y a quelques années cependant, elles étaient au nombre de : ";
-		System.out.println(texte1 + nbPlanete + texte2 + (++nbPlanete));
-		
+		String phraseIntroduction = "En %d, les planètes du système solaire était au nombre de : %d";
+		short annee = 2006;
+		int nbPlanete = 0;
+
+		if (annee < 2006) {
+			nbPlanete = 9;
+		}
+
+		if (annee >= 2006) {
+			nbPlanete = 8;
+		}
+
+		System.out.printf(phraseIntroduction, annee, nbPlanete);
+
 	}
 
 }
