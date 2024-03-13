27899d3c0d06e73875a98302b9867e40e60472a2
D-Rudy <duriez.ry@gmail.com>
2024-03-13 16:26:57 +0100

    if imbriqué

--- com.rdz.HelloUniverse/src/com/rdz/HelloUniverse/HelloUniverse.java
+++ com.rdz.HelloUniverse/src/com/rdz/HelloUniverse/HelloUniverse.java
@@ -4,18 +4,25 @@ public class HelloUniverse {
 	public static void main(String[] args) {
 
 		String phraseIntroduction = "En %d, les planètes du système solaire était au nombre de : %d";
-		short annee = 2006;
+		String phraseErreur = "Le programme ne peut pas fournir de résultat pour l'année %d";
+		int annee = 2006;
 		int nbPlanete = 0;
 
-		if (annee < 2006) {
-			nbPlanete = 9;
-		}
+		if (annee < 1600 || annee > 2020) {
+			System.out.printf(phraseErreur, annee);
+		} else {
+			if (annee < 1700) {
+				nbPlanete = 7;
+			} else if (annee < 1800 || annee >= 2006) {
+				nbPlanete = 8;
+			} else if (annee < 2006) {
+				nbPlanete = 9;
+			} else {
+				nbPlanete = 8;
+			}
 
-		if (annee >= 2006) {
-			nbPlanete = 8;
+			System.out.printf(phraseIntroduction, annee, nbPlanete);
 		}
-
-		System.out.printf(phraseIntroduction, annee, nbPlanete);
 
 	}
 
