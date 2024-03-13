2538ec460ac2056140f47f0af36e1b0d24f4a306
D-Rudy <duriez.ry@gmail.com>
2024-03-13 17:15:53 +0100

    Switch Case

--- com.rdz.HelloUniverse/src/com/rdz/HelloUniverse/HelloUniverse.java
+++ com.rdz.HelloUniverse/src/com/rdz/HelloUniverse/HelloUniverse.java
@@ -5,23 +5,29 @@ public class HelloUniverse {
 
 		String phraseIntroduction = "En %d, les planètes du système solaire était au nombre de : %d";
 		String phraseErreur = "Le programme ne peut pas fournir de résultat pour l'année %d";
+		//
+		//
+		//
+		//
+
 		int annee = 2006;
-		int nbPlanete = 0;
+		int nbPlanete = 7;
 
-		if (annee < 1600 || annee > 2020) {
-			System.out.printf(phraseErreur, annee);
-		} else {
-			if (annee < 1700) {
-				nbPlanete = 7;
-			} else if (annee < 1800 || annee >= 2006) {
-				nbPlanete = 8;
-			} else if (annee < 2006) {
-				nbPlanete = 9;
-			} else {
-				nbPlanete = 8;
-			}
-
-			System.out.printf(phraseIntroduction, annee, nbPlanete);
+		switch (nbPlanete) {
+		case 7:
+			System.out.printf("On sait qu'au 16ème siècle, seules 7 planètes avaient été découvertes");
+			break;
+		case 8:
+			System.out.printf(
+					"On sait que le nombre de planètes est passé de 7 à 8 au 17ème siècle, mais il a également été réduit de 9 à 8 en 2006");
+			break;
+		case 9:
+			System.out.printf(
+					"On sait que le nombre de planètes est passé de 8 à 9 au 18ème siècle et ce jusqu'en 2006, où ce nombre a été réduit à 8");
+			break;
+		default:
+			System.out.printf("Le programme ne peut pas fournir de résultat pour %d planètes", nbPlanete);
+			break;
 		}
 
 	}
