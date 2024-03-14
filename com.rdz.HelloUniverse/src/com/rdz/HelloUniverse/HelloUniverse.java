5eaa3f6ba94c52de46e5eb08d7447bd7920de336
D-Rudy <duriez.ry@gmail.com>
2024-03-14 15:35:24 +0100

    Classe et instances de classes

--- com.rdz.HelloUniverse/src/com/rdz/HelloUniverse/HelloUniverse.java
+++ com.rdz.HelloUniverse/src/com/rdz/HelloUniverse/HelloUniverse.java
@@ -2,27 +2,47 @@ package com.rdz.HelloUniverse;
 
 public class HelloUniverse {
 	public static void main(String[] args) {
-		int nbPlanete = 7;
-		while ( nbPlanete < 10) {
-			switch (nbPlanete) {
-			case 7:
-				System.out.println("On sait qu'au 16ème siècle, seules 7 planètes avaient été découvertes");
-				break;
-			case 8:
-				System.out.println(
-						"On sait que le nombre de planètes est passé de 7 à 8 au 17ème siècle, mais il a également été réduit de 9 à 8 en 2006");
-				break;
-			case 9:
-				System.out.println(
-						"On sait que le nombre de planètes est passé de 8 à 9 au 18ème siècle et ce jusqu'en 2006, où ce nombre a été réduit à 8");
-				break;
-			default:
-				System.out.printf("Le programme ne peut pas fournir de résultat pour %d planètes", nbPlanete);
-				break;
-			}
-			nbPlanete++;
-		}
+		Planet mercury = new Planet();
+		mercury.name = "mercury";
+		mercury.diameter = 4879L;
+		mercury.material = "Telluric";
 
+		Planet venus = new Planet();
+		venus.name = "Venus";
+		venus.diameter = 12104L;
+		venus.material = "Telluric";
+
+		Planet earth = new Planet();
+		earth.name = "Earth";
+		earth.diameter = 12742L;
+		earth.material = "Telluric";
+
+		Planet mars = new Planet();
+		mars.name = "Mars";
+		mars.diameter = 6779L;
+		mars.material = "Telluric";
+
+		Planet jupiter = new Planet();
+		jupiter.name = "Jupiter";
+		jupiter.diameter = 139822L;
+		jupiter.material = "Gaseous";
+
+		Planet saturn = new Planet();
+		saturn.name = "Saturn";
+		saturn.diameter = 116460L;
+		saturn.material = "Gaseous";
+
+		Planet uranus = new Planet();
+		uranus.name = "Uranus";
+		uranus.diameter = 50724L;
+		uranus.material = "Gaseous";
+
+		Planet neptune = new Planet();
+		neptune.name = "Neptune";
+		neptune.diameter = 49244L;
+		neptune.material = "Gaseous";
+
+		System.out.println("Planet: " + jupiter.name + "\ndiameter: " + jupiter.diameter + " km \nmaterial: " + jupiter.material);
 	}
 
 }
